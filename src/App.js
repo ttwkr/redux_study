36c2866c676e8df5eee71cbeb74f5c2720cf4eb3
ttwkr <ttwkor@naver.com>
2020-01-18 22:06:50 +0900

    redux study first setting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import "./App.css";
+
+const GlobalStyled = createGlobalStyle`
+div{
+  border : 5px solid #764abc;
+  margin : 10px;
+  color : #764abc;
+  padding : 10px;
+}
+`;
+
+class AddNumber extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Number</h1>
+        <input type="button" value="+"></input>
+        <input type="text" value="0"></input>
+      </div>
+    );
+  }
+}
+
+class AddNumberRoot extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Number Root</h1>
+        <AddNumber></AddNumber>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyled />
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot></AddNumberRoot>
+        <DisplayNumberRoot></DisplayNumberRoot>
+      </div>
+    </>
   );
+}
+
+class DisplayNumber extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dispaly Number</h1>
+        <input type="text" value="0" readOnly></input>
+      </div>
+    );
+  }
+}
+
+class DisplayNumberRoot extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DisplayNumber Root</h1>
+        <DisplayNumber></DisplayNumber>
+      </div>
+    );
+  }
 }
 
 export default App;
