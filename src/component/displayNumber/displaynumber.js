35f565cd8e5aea75317e25ac6b804401d26ad702
ttwkr <ttwkor@naver.com>
2020-01-18 23:09:29 +0900

    리덕스 적용

--- src/component/displayNumber/displaynumber.js
+++ src/component/displayNumber/displaynumber.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-
+import store from "../../store";
 class DisplayNumber extends Component {
+  state = { number: store.getState().number };
+  constructor(props) {
+    super(props);
+    store.subscribe(() => {
+      this.setState({
+        number: store.getState().number
+      });
+    });
+  }
   render() {
     return (
       <div>
         <h1>Dispaly Number</h1>
-        <input type="text" value={this.props.number} readOnly></input>
+        <input type="text" value={this.state.number} readOnly></input>
       </div>
     );
   }
